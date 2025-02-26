9400a78a11faf0e4033ce94e3ae94df3a193bbf4
hervid2 <hdcardonab@gmail.com>
2025-02-25 21:20:48 -0500

    25-02-2025

--- 2_nodos/3_atributos/app.js
+++ 2_nodos/3_atributos/app.js
@@ -1,19 +1,82 @@
-const input = document.querySelector("input");
-const contraseña = document.querySelector('input[type="password');
-console.log(input);
-console.log(input.type);
-console.log(input.value);
-console.log(input.placeholder);
-console.log(contraseña);
-input.value = "John Fredy";
-console.log(contraseña.value ? 'lleno':'vacío');
-contraseña.value = "123456";
-contraseña.setAttribute("type","text" ); 
+const body = document.querySelector("body");
+const formulario = document.querySelector("#formulario");
+const nombre = document.getElementById("nombre");
+const apellido = document.querySelector("#apellido");
+const telefono = document.querySelector("#telefono");
+const documento = document.querySelector("#documento");
+const usuario = document.querySelector("#usuario");
+const contrasena = document.querySelector("#contrasena");
+const btn = document.querySelector("button");
+const menuDesplegable = document.querySelector("#card");
+
+const ciudades = async () => {
+   const data = await fetch('data.json');
+   const ciudades = await data.json();
+//    console.log(ciudades); 
+ciudades.array.forEach(element => {
+    console.log(element);
+});
+};
+ciudades();
+
+formulario = document.createElement("div");
+formulario = document.add("select");
+formulario = document.append();
 
 
+// validar un formulario
+const validar = (event) => {
+  // detenemos el evento
+  event.preventDefault();
+
+  // validamos los campos
+  if (nombre.value == "") {
+    alert("El nombre es obligatorio");
+    nombre.focus();
+  } 
+  if (apellido.value == "") {
+    alert("El apellido es obligatorio");
+    apellido.focus();   
+    }
+    if (telefono.value == "") {
+        alert("El telefono es obligatorio");
+        telefono.focus();
+        }
+        if (documento.value == "") {
+            alert("El documento es obligatorio");
+            documento.focus();
+            }
+            if (usuario.value == "") {
+                alert("El usuario es obligatorio");
+                usuario.focus();
+                }
+                if (contrasena.value == "") {
+                    alert("La contraseña es obligatoria");
+                    contrasena.focus();
+                    }
+};
+btn.addEventListener('click', validar);
+// btn.removeEventListener('click',validar);
+
+const  contextMenu = () => {
+    alert("?");
+}
+// formulario.addEventListener('contextMenu',contextMenu);
+
+const dblclick = () => {
+    alert("doble click");
+}
+// body.addEventListener('dblclick',dblclick);
 
 
+const mousedown = () => {
+    alert("El evento funciona cuando se mantiene presiono cualquier click sobre el elemento")
+}
+// body.addEventListener('mousedown',mousedown);
 
-
-
+const keydown = (event) => {
+    alert(`Presionaste la tecla ${event.key}`);
+}
+//  nombre.addEventListener('keydown',keydown);
+// formulario.addEventListener('',);
 
